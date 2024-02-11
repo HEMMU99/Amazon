2b379749385dad2e4011533a74d2e6284e0fbd6d
PRANAYRAJ2804 <pranayraj2804@gmail.com>
2024-02-11 16:54:37 +0530

    currency format removed

--- src/Components/Subtotal/SubTotal.jsx
+++ src/Components/Subtotal/SubTotal.jsx
@@ -1,30 +1,21 @@
 import React from "react";
 import "./SubTotal.css";
-import CurrencyFormat from "react-currency-format";
 import { contextValue } from "../Context/StateProvider";
 import { getCartTotal } from "../Context/Reducer";
 const SubTotal = () => {
   const [{ cart }, dispatch] = contextValue();
+  const subtotalValue = getCartTotal(cart).toFixed(2);
+  const formattedSubtotal = `₹${subtotalValue}`;
   return (
     <div className="subtotal">
-      <CurrencyFormat
-        renderText={(value) => (
-          <>
-            <p>
-              Subtotal ({cart.length} items):
-              <strong>{value}</strong>
-            </p>
-            <small className="subtotal-gift">
-              <input type="checkbox" name="" id="" /> This order contains a gift
-            </small>
-          </>
-        )}
-        decimalScale={2}
-        value={getCartTotal(cart)}
-        displayType={"text"}
-        thousandSeparator={true}
-        prefix={"₹"}
-      />
+      <p>
+        Subtotal ({cart.length} items):
+        <strong>{formattedSubtotal}</strong>
+      </p>
+      <small className="subtotal-gift">
+        <input type="checkbox" name="" id="" /> This order contains a gift
+      </small>
+
       <button>Proceed to Checkout</button>
     </div>
   );
